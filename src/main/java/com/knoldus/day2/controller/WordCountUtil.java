50904910a0de50f966ba8b30fc305887d68eba6c
surajsaini95 <surajkumar90@rocketmail.com>
2020-03-04 23:03:58 +0530

    added-assignment6

---
+++ src/main/java/com/knoldus/day2/controller/WordCountUtil.java
@@ -0,0 +1,29 @@
+package com.knoldus.day2.controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class WordCountUtil {
+    public Map<String, Integer> getWordCountOfDataFromFile(String filePath) {
+        Map<String, Integer> result = new HashMap<>();
+        try (Stream<String> lines = Files.lines(Path.of(filePath))) {
+            lines.forEach(line -> Arrays.stream(line.split(" "))
+                    .forEach(str -> {
+                        int one =1;
+                        if (result.containsKey(str))
+                            result.replace(str, result.get(str) +one);
+                        else
+                            result.put(str, one);
+                    }));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+}
--- src/main/java/com/knoldus/day2/view/AppDriver.java
+++ src/main/java/com/knoldus/day2/view/AppDriver.java
@@ -2,11 +2,13 @@ package com.knoldus.day2.view;
 
 import com.knoldus.day2.controller.DateAndTimeUtil;
 import com.knoldus.day2.controller.StudentUtil;
+import com.knoldus.day2.controller.WordCountUtil;
 
 public class AppDriver {
     public static void main(String[] args) {
-        StudentUtil studentUtil=new StudentUtil();
-        DateAndTimeUtil dateAndTimeUtil=new DateAndTimeUtil();
+        StudentUtil studentUtil = new StudentUtil();
+        DateAndTimeUtil dateAndTimeUtil = new DateAndTimeUtil();
+        WordCountUtil wordCountUtil = new WordCountUtil();
 
         /*System.out.println(studentUtil.getStudentsWithoutSecondaryAddress(StudentUtil.getListOfStudents()));
         System.out.println(studentUtil.getStudentsWithoutSecondaryAddressAndNameFilter(StudentUtil.getListOfStudents()));
@@ -23,7 +25,11 @@ public class AppDriver {
         String birthDate="2020-01-01";
         String deathDate="2020-01-02";
         System.out.println(dateAndTimeUtil.getLifeDurationInSeconds(birthDate,deathDate));
-*/
+
         System.out.println(dateAndTimeUtil.getLeapYearsAfterFirstRepublicDay());
+
+         */
+        String filePath = "./src/main/resources/data";
+        System.out.println(wordCountUtil.getWordCountOfDataFromFile(filePath));
     }
 }
